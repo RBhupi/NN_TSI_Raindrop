0de90b17032a5270b843c7c73932c628fc0cdbfb
Bhupendra Raut <bhupendra.raut@northwestern.edu>
2022-01-26 13:21:30 -0600

    dirty code for making label-data

--- save_training_files1.py
+++ save_training_files1.py
@@ -6,7 +6,7 @@ Created on Thu Jan 20 16:25:48 2022
 import numpy as np
 import cv2 as cv
 import pandas as pd
-
+from os.path import basename
 
 
 def openVideoFile(fname):
@@ -21,13 +21,14 @@ def openVideoFile(fname):
 
 def readVideoFrame(fcount, capture):
     ret, frame = capture.read()
-    fcount += 1 
 
     if not ret:
         capture.release()
         print("End of video reached!")
-        return -1, frame
+        return fcount, frame
+    
     frame = frame[140:500,70:430,]
+    fcount += 1 
     return fcount, frame
 
 
@@ -35,43 +36,51 @@ def readVideoFrame(fcount, capture):
 
 
 #crop video data
-video_name = "/Users/bhupendra/projects/cloud_motion/data/waggle-scott-tsi/sgptsimovieC1.a1.20170102.000000.mpg"
+video_name = "/Users/bhupendra/projects/cloud_motion/data/waggle-scott-tsi/sgptsimovieC1.a1.20210109.000000.mpg"
 
-data_dir = "/Users/bhupendra/projects/camera_raindrops/data/train_data1/"
+data_dir = "/Users/bhupendra/projects/camera_raindrops/data/train_data5/"
 
 video_cap = openVideoFile(video_name)
 
 file_list = []
 
 fcount=0
+date_str = "2021-01-09_"
 
 while True:
     fcount, frame = readVideoFrame(fcount, video_cap)
     
-    #Rain contamination period
-    if fcount <= 360:
-        img_path = data_dir + str(fcount) + ".jpg"
-        cv.imwrite(img_path, frame)
-        file_list.append(img_path)
     
-    #clean mirror period
-    if fcount > 540 and fcount <= 900:
-        img_path = data_dir + str(fcount) + ".jpg"
+    #Rain contamination period
+    if fcount <= 220:
+        img_path = data_dir + date_str + str(fcount) + ".jpg"
         cv.imwrite(img_path, frame)
-        file_list.append(img_path)
-    if fcount >= 900:
+        file_list.append(basename(img_path))
+    
+    """#clean mirror period
+    if fcount > 540 and fcount <= 900:
+        img_path = data_dir + date_str + str(fcount) + ".jpg"
+        cv.imwrite(img_path, frame)
+        file_list.append(basename(img_path))"""
+    if fcount >= 220:
         break
 
 
+    #img_path = data_dir + date_str + str(fcount) + ".jpg"
+    #cv.imwrite(img_path, frame)
+    #file_list.append(basename(img_path))
 
 
-
-labels = [1 if x<360 else 0 for x in range(720)]
+#labels = [1 if x<360 else 0 for x in range(720)]
+labels = [2 for x in range(220)]
 data = {'files':file_list, 'labels':labels}
-
-
 df = pd.DataFrame(data=data)
-df.to_csv(data_dir+"labels.csv")
+csv_name = data_dir + "labels.csv"
+
+with open(csv_name, 'a') as f:
+    df.to_csv(f, mode='a', header=f.tell()==0, index=False)
+
+
 
 
 
